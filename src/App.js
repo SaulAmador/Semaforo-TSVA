9ebefe6f0d12bf5304e0373f835e64616bfce5d2
Saul Amador <anthraz.wow@gmail.com>
2025-03-23 23:39:54 -0600

    ProyectoSemaforo-TSVA

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import TrafficLight from './TrafficLight';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <TrafficLight />
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
